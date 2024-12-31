8a516a5086de1b946b34166e1c36fd3250f2ab2a
Malik Abubakar Shafeeq <abubakar.shafeeq@itcs.com.pk>
2024-12-31 12:06:31 +0500

    new sections

---
+++ src/Components/Home/Section2/Section2.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./section2.scss";
+import CloudLogo from "./../../../assets/logos/Cloud.png"
+const Section2 = () => {
+  return (
+    <>
+      <div className="section2 ">
+        <div className="cards">
+          <div className="card" >
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">MODERN WORKPLACE</h5>
+              <p className="card-text">
+                Unleash the potential of modern work with an array of
+                transformative solutions.
+              </p>
+            </div>
+            <div className="card-body">
+              <a href="#" className="card-link">
+                LEARN MORE
+              </a>
+            </div>
+          </div>
+          <div className="card" >
+            <img src={CloudLogo} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">CLOUD COMPUTING</h5>
+              <p className="card-text">
+                We are equipped to guide you through the transformative
+                potential of Azure.
+              </p>
+            </div>
+            <div className="card-body">
+              <a href="#" className="card-link">
+                LEARN MORE
+              </a>
+            </div>
+          </div>
+          <div className="card" >
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">SOFTWARE LICENSING</h5>
+              <p className="card-text">
+                Choose the licensing option that aligns seamlessly with your
+                business rhythm.
+              </p>
+            </div>
+            <div className="card-body">
+              <a href="#" className="card-link">
+                LEARN MORE
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Section2;
---
+++ src/Components/Home/Section3/Section3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Section3.scss"
+const Section3 = () => {
+  return (
+    <>
+    <div>ggggg</div>
+    </>
+  )
+}
+
+export default Section3
