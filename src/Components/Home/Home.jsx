2d9d3a1d61a07a1ba207a05ee8e64de3b5383cab
Malik Abubakar Shafeeq <abubakar.shafeeq@itcs.com.pk>
2025-01-06 13:02:13 +0500

    section7

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,6 +5,7 @@ import Section3 from './Section3/Section3'
 import Section4 from './Section4/Section4'
 import Section5 from './Section5/Section5'
 import Section6 from './Section6/Section6'
+import Section7 from './Section7/Section7'
 const Home = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const Home = () => {
       <Section4/>
       <Section5/>
       <Section6/>
+      <Section7/>
 
     </>
     
