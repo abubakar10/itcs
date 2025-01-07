ca3c24feeeeca25b322b94c0fb80543bb106281b
Malik Abubakar Shafeeq <abubakar.shafeeq@itcs.com.pk>
2025-01-07 14:42:47 +0500

    services

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./Services.scss";
 import ServicesSection1 from "./ServicesSection1/ServicesSection1";
 import ServiceSection2 from "./ServiceSection2/ServiceSection2";
-
+import ServiceSection3 from "./ServiceSection3/ServiceSection3";
 const Services = () => {
   return (
     <>
       
       <ServicesSection1/>
       <ServiceSection2/>
+      <ServiceSection3/>
+
 
       </>
     
