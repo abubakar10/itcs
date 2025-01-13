04e58c1911ce6ca253f9cff1255cbaf518735386
Malik Abubakar Shafeeq <abubakar.shafeeq@itcs.com.pk>
2025-01-13 17:18:55 +0500

    Company Profile

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -3,11 +3,13 @@ import Asection1 from './Asection1/Asection1'
 import Section6 from '../Home/Section6/Section6'
 import Section7 from "./../Home/Section7/Section7"
 import Asection2 from './Asection2/Asection2'
+import Asection3 from './Asection3/Asection3'
 const AboutUs = () => {
   return (
     <>
     <Asection1/>
     <Asection2/>
+    <Asection3/>
     <Section6/>
     <Section7/>
     </>
