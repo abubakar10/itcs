0e74577789a2263283d13f6bc9e7e00fd59a13e0
Wajeeha-Zeeshan <130417010+Wajeeha-Zeeshan@users.noreply.github.com>
2025-11-14 18:55:03 +0800

    Private routing
    Corrected the private routing

--- src/Backend/models/userModel.js
+++ src/Backend/models/userModel.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  
+  role: { type: String, default: 'admin' },      
+  isAdmin: { type: Boolean, default: true }, 
 })
 
 export default mongoose.model('User', userSchema)
